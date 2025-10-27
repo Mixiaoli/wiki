df4ded1e349b2621bde8171f63f7693fcfe7ddf3
Mixaioli <2324598590@qq.com>
2025-10-27 18:57:13 +0800

    10.27 修改前端页面

--- web/src/views/home.vue
+++ web/src/views/home.vue
@@ -82,7 +82,8 @@ import TheWelcome from '@/components/the-welcome.vue';
 //     description:
 //         'Ant Design, a design language for background applications, is refined by Ant UED Team.',
 //     content:
-//         'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+//         'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to
+//help people create their product prototypes beautifully and efficiently.',
 //   });
 // }
 
